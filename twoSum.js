4a4a0e390bf35919d63a569e51a2c426046bd407
Raytene <raytenehan@gmail.com>
2022-10-31 12:05:34 -0700

    O(n) solution with map

--- twoSum.js
+++ twoSum.js
@@ -3,10 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length - 1; i++) {
-      for (let j = 1; j < nums.length; j++) {
-          if (nums[i] + nums[j] === target && i !== j) return [i, j];
-      }
+var twoSum = function(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+
+    const difference = target - nums[i];
+
+    if (map.has(difference)) {
+      return [i, map.get(difference)];
+    }
+
+    map.set(nums[i], i);
   }
 };
