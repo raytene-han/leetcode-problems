367e045c306f9aeb3837ef1690629984b406fc2a
Raytene <raytenehan@gmail.com>
2022-11-13 18:53:31 -0800

    solve invertBinaryTree recursively

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -10,22 +10,38 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// RECURSIVE SOLUTION
  var invertTree = function(root) {
-  if (!root || (!root.left && !root.right)) return root;
+  if (!root) return null;
 
-  let currentNode = root;
-  let queue = [currentNode];
-
-  while (queue.length > 0) {
-      if (currentNode !== null && (currentNode.left || currentNode.right)) {
-          const temp = currentNode.left;
-          currentNode.left = currentNode.right;
-          currentNode.right = temp;
-          queue.push(currentNode.left, currentNode.right);
-      }
-      queue.shift();
-      currentNode = queue[0];
-  }
-
+  [root.left, root.right] = [root.right, root.left];
+  invertTree(root.left);
+  invertTree(root.right);
   return root;
 };
+
+// ITERATIVE SOLUTION
+//  var invertTree = function(root) {
+//   if (!root || (!root.left && !root.right)) return root;
+
+//   let currentNode = root;
+//   let queue = [currentNode];
+
+//   while (queue.length > 0) {
+//       if (currentNode !== null && (currentNode.left || currentNode.right)) {
+//           const temp = currentNode.left;
+//           currentNode.left = currentNode.right;
+//           currentNode.right = temp;
+//           queue.push(currentNode.left, currentNode.right);
+//       }
+//       queue.shift();
+//       currentNode = queue[0];
+//   }
+
+//   return root;
+// };
