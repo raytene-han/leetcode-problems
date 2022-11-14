9c55c0b18df420b3ad48c10cec0767dd0eeb70fd
Raytene <raytenehan@gmail.com>
2022-11-13 18:50:24 -0800

    solve invertBinaryTree iteratively

---
+++ invertBinaryTree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+ var invertTree = function(root) {
+  if (!root || (!root.left && !root.right)) return root;
+
+  let currentNode = root;
+  let queue = [currentNode];
+
+  while (queue.length > 0) {
+      if (currentNode !== null && (currentNode.left || currentNode.right)) {
+          const temp = currentNode.left;
+          currentNode.left = currentNode.right;
+          currentNode.right = temp;
+          queue.push(currentNode.left, currentNode.right);
+      }
+      queue.shift();
+      currentNode = queue[0];
+  }
+
+  return root;
+};
